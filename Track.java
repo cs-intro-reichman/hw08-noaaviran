19c8f05aad21194ca2960496fee5c75270e69469
Avielni222 <155298746+Avielni222@users.noreply.github.com>
2024-02-20 10:51:24 +0200

    Update Track.java

--- Track.java
+++ Track.java
@@ -43,6 +43,8 @@ class Track {
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
         //// replace the following statement with your code
+
+        // where is the solution?
         return "";
     }
 }
